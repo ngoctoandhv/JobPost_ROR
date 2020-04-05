e073ff453d54a9a822bf5b46df0d0d4cd1787574
ngoctoandhv95 <ngoctoandhv95@gmail.com>
2020-04-05 17:07:18 +0700

    add user_name and user_type to model user and chagne view sigup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :user_name, presence: true
+  validates :user_type, presence: true
+  validates :user_type, inclusion: {in: %w(Candidate Employer),allow_blank: true}
+
+  USER_TYPE = %w{ Candidate Employer }
 end
